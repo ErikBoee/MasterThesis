36e27d73c5f082d4b94002ff0ecd833496fe6c36
ErikBoee <erikoscarboe@gmail.com>
2020-12-17 12:59:25 +0100

    delete test

--- source/idun_run.py
+++ source/idun_run.py
@@ -1,7 +1,7 @@
 import numpy as np
 import utilities_running as ur
 
-max_iterator = 10
+max_iterator = 10000
 filename = "Problems/Circle_test_well_approximation.npy"
 problem_dictionary = np.load(filename, allow_pickle=True).item()
 opt_object = ur.get_opt_object_from_problem_dictionary(problem_dictionary, max_iterator)
