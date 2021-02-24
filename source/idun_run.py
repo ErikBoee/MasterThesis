9205e92064941084309d8d5445dede56bd527029
ErikBoee <erikoscarboe@gmail.com>
2021-02-24 13:27:30 +0100

    paal 2.0

--- source/idun_run.py
+++ source/idun_run.py
@@ -4,13 +4,13 @@ import constants as const
 import os
 
 problem_dictionary_filename = "Problems/Circle_not_fine_grid_update_reference.npy"
-new_folder_name = "Circle_not_fine_grid_update_reference_test"
+new_folder_name = "Circle_not_fine_grid_update_reference_alternating_regularization"
 new_path = r'../source/' + new_folder_name
 if not os.path.exists(new_path):
     os.makedirs(new_path)
 directory = new_path
-max_iterator = 1
-image_frequency = 1
+max_iterator = 50
+image_frequency = 10
 problem_dictionary = np.load(problem_dictionary_filename, allow_pickle=True).item()
 opt_object = ur.get_opt_object_from_problem_dictionary_bfgs(problem_dictionary, max_iterator, image_frequency)
 ur.test_bfgs_method(problem_dictionary, opt_object, new_folder_name, new_path)
