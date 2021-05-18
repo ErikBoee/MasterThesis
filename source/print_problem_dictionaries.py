f32802e18efa92b358e472953e5bb057fa7ac207
ErikBoee <erikoscarboe@gmail.com>
2021-05-18 20:41:49 +0200

    Experiment 7

--- source/print_problem_dictionaries.py
+++ source/print_problem_dictionaries.py
@@ -5,7 +5,7 @@ for j in range(10):
     for lamda in [100, 1000, 10000, 100000]:
         for i in range(6):
             filename = "j_" + str(j) + "_lambda_" + str(lamda) + "_i_" + str(int(i)) + ".npy"
-            path = "Experiments_finished/Experiment_5/Experiment_5_noise_0_15_beta_1_6_no_angles_16_lambda_100_1000000/" + filename
+            path = "Experiments_finished/Experiment_6/Experiment_6_noise_0_beta_1_6_no_angles_16_lambda_100_1000000/" + filename
             if os.path.exists(path):
                 problem_dictionary = np.load(path,
                                              allow_pickle=True).item()
